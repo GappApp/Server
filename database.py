549c88c9ff1d73178825716ec4bc1fe95a2c78c9
Amir Khazaie <733amir@gmail.com>
2017-08-22 10:50:55 +0430

    First version of the project!

---
+++ database.py
@@ -0,0 +1,60 @@
+from sqlite3 import connect
+
+
+class Database:
+    def __init__(self):
+        # Our Database of Users
+        self.user_db = connect('users.db')
+        self.ucur = self.user_db.cursor()
+        self.ucur.execute('drop table users')
+        self.ucur.execute('create table users (username text)')
+        self.user_db.commit()
+
+        # Our Database of Messages
+        self.message_db = connect('messages.db')
+        self.mcur = self.message_db.cursor()
+        self.mcur.execute('drop table messages')
+        self.mcur.execute('create table messages '
+                          '(sender text, receiver text,'
+                          ' message text)')
+        self.message_db.commit()
+
+    def username_exist(self, username):
+        for row in self.ucur.execute('select * from users'):
+            if row[0] == username:
+                return True
+        return False
+
+    def add_username(self, username):
+        self.ucur.execute('insert into users values ("{}")'.format(username))
+        self.user_db.commit()
+
+    def remove_username(self, username):
+        self.ucur.execute('delete from users where username={}'.format(username))
+
+    def get_all_usernames(self):
+        self.ucur.execute('select * from users')
+        return self.ucur.fetchall()
+
+    def get_all_messages(self):
+        self.mcur.execute('select * from messages')
+        return self.mcur.fetchall()
+
+    def send_message(self, sender, receiver, message):
+        self.mcur.execute('insert into messages values ("{}", "{}", "{}")'.format(sender, receiver, message))
+        self.message_db.commit()
+
+    def get_mates(self, username):
+        mates = [username]
+        for row in self.mcur.execute('select * from messages where sender="{0}" or receiver="{0}"'.format(username)):
+            if row[0] not in mates:
+                mates.append(row[0])
+            if row[1] not in mates:
+                mates.append(row[1])
+        mates.remove(username)
+        return mates
+
+    def get_messages(self, mate1, mate2):
+        self.mcur.execute('select * from messages where (sender="{0}" and receiver="{1}") or'
+                          ' (receiver="{0}" and sender="{1}")'.format(mate1, mate2))
+        return self.mcur.fetchall()
---
+++ server.py
@@ -0,0 +1,64 @@
+from flask import Flask
+from database import Database
+
+# Web Server Application and Database Manager
+app = Flask(__name__)
+db = Database()
+
+
+@app.route('/sign_in/<username>/')
+def sign_in(username):
+    if not db.username_exist(username):
+        db.add_username(username)
+        return '{"Status" : "Ok."}'
+    else:
+        return '{"Status" : "Error!", "Description" : "Username exist."}'
+
+
+@app.route('/sign_out/<username>')
+def sign_out(username):
+    if db.username_exist(username):
+        db.remove_username(username)
+        return '{"Status": "Ok."}'
+    else:
+        return '{"Status" : "Error!", "Description" : "Username don\'t exist."}'
+
+
+@app.route('/send/<sender>/<receiver>/<message>')
+def send(sender, receiver, message):
+    if db.username_exist(sender):
+        if db.username_exist(receiver):
+            db.send_message(sender, receiver, message)
+            return '{"Status" : "Ok."}'
+        else:
+            return '{"Status" : "Error!", "Description" : "Receiver username does not exist."}'
+    else:
+        return '{"Status" : "Error!", "Description" : "Sender username does not exist."}'
+
+
+@app.route('/get_mates/<username>')
+def get_mates(username):
+    if db.username_exist(username):
+        mates = db.get_mates(username)
+        return '{{"Status" : "Ok.", "Mates" : {}}}'.format(list(mates))
+    else:
+        return '{"Status" : "Error!", "Description" : "Username does not exist."}'
+
+
+@app.route('/get_messages/<mate1>/<mate2>')
+def get_messages(mate1, mate2):
+    if db.username_exist(mate1) and db.username_exist(mate2):
+        messages = db.get_messages(mate1, mate2)
+        return '{{"Status" : "Ok.", "Messages" : {}}}'.format(messages)
+    else:
+        return '{"Status" : "Error!", "Description" : "Username does not exist."}'
+
+
+@app.route('/debug')
+def debug():
+    users = db.get_all_usernames()
+    messages = db.get_all_messages()
+    return 'users: {} <br> <br> messages: {}'.format(users, messages)
+
+
+app.run('127.0.0.1', 5000)
